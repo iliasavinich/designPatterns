5fe75d131fe223c1b35a5f6853e7fdfd1b46695e
ilyasavinich <savinich228@gmail.com>
2020-01-15 13:07:20 +0300

    feature/rebase-test - replaced primitive type by Wrapper

--- src/main/java/behavioralPatterns/command/Stock.java
+++ src/main/java/behavioralPatterns/command/Stock.java
@@ -3,7 +3,7 @@ package behavioralPatterns.command;
 public class Stock {
 
     private String name = "ABC";
-    private int quantity = 10;
+    private Integer quantity = 10;
 
     public void buy(){
         System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] bought");
